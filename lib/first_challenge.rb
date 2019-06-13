012447a5e02e98e4a66ef8d06567a791497a5e74
Iris S <irisiseve@gmail.com>
2019-06-13 19:16:28 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,13 @@
 def first_challenge
    epic_tragedy = {
+<<<<<<< HEAD
       :montague => {},
       :capulet => {}
    }
 end
+=======
+     :montague => {} ,
+     :capulet => {}
+   }
+end
+>>>>>>> f4230492a494cbc1e7e9948f95da18a27b023531
